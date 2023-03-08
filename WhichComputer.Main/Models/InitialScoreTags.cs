9930d1461b2a13781381f4352cc48e8f50c979da
Peyton Winn <pwinn@clemson.edu>
2023-03-08 15:02:21 -0500

    started work on scoring CS files

---
+++ WhichComputer.Main/Models/InitialScoreTags.cs
@@ -0,0 +1,44 @@
+﻿namespace WhichComputer.Main.Models
+{
+    public class InitialScoreTags
+    {
+
+        public bool Novice { get; set; }
+
+        public bool Intermediate { get; set; }
+
+        public bool Expert { get; set; }
+
+        public bool Laptop { get; set; }
+
+        public bool Desktop { get; set; }
+
+        public bool LowUse { get; set; }
+
+        public bool Office { get; set; }
+
+        public bool BrowserFocused { get; set; }
+
+        public bool Minimal { get; set; }
+
+        public bool Lightweight { get; set; }
+
+        public bool HighUse { get; set; }
+
+        public bool Professional { get; set; }
+
+        public int Graphics { get; set; }
+
+        public bool Gaming { get; set; }
+
+        public bool SmallStorage { get; set; }
+
+        public bool MediumStorage { get; set; }
+
+        public bool HighStorage { get; set; }
+
+        public bool NoChromebook { get; set; }
+
+        public int PriceRange { get; set; } // 1 - 7
+    }
+}
---
+++ WhichComputer.Main/Models/ScoringCalculation.cs
@@ -0,0 +1,16 @@
+﻿using WhichComputer.Main.Models;
+
+namespace WhichComputer.Main
+{
+    public class ScoringCalculation
+    {
+        public static FinalScore CalculateScore(InitialScoreTags initialScore)
+        {
+            FinalScore finalScore = new FinalScore();
+
+
+
+            return finalScore;
+        }
+    }
+}
